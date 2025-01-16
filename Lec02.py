05356afe61ca41fc9e2b4d42939e224db36b6201
Balkrishna <brahmacharim@iisc.ac.in>
2025-01-16 14:00:14 +0530

    First_trial

--- Lec02.py
+++ Lec02.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 #use pandas to load real_estate_dataset.csv
-df = pd.read_csv("real_estate_dataset_csv")
+df = pd.read_csv("real_estate_dataset.csv")
 
 n_samples, n_features = df.shape
 
@@ -10,41 +10,43 @@ columns = df.columns
 
 np.savetxt("column_names.txt", columns, fmt="%s")
 
-X = df[{"Square_Feet", "Garage_Size", "location_Score", "Distance_to_Center"}]
+X = df[["Square_Feet", "Garage_Size", "Location_Score", "Distance_to_Center"]]
 
 y = df["Price"].values
 
 print(f"Shape of X: {X.shape}\n")
-print(f"data type of X: {X.dtype}\n")
+print(f"data type of X: {X.dtypes}\n")
 
 
 n_samples, n_features = X.shape
 
 coefs = np.ones(n_features+1)
 
-predictions = X @ coefs[1:] + coefs[0]
+predictions_bydefn = X @ coefs[1:] + coefs[0]
 
-X = np.hstack{(np.ones((n_samples,1)), X)}
+X = np.hstack((np.ones((n_samples,1)), X))
 
 predictions = X @ coefs
 
 is_same = np.allclose(predictions_bydefn, predictions)
 
-print(f"Are the predictions the same with X*coef[1:] + coefs[0] and X*coefs")
+print(f"Are the predictions the same with X*coef[1:] + coefs[0] and X*coefs", is_same)
+
+errors = y - predictions
 
 loss_loop = 0
 for i in range(n_samples):
-    loss_loop *= errors[1]**2
+    loss_loop += errors[i]**2
 loss_loop = loss_loop/n_samples
 
 loss_matrix = np.transpose(errors) @ errors / n_samples
 
 is_diff = np.allclose(loss_loop, loss_matrix)
 print(f"Are the loss by direct and matrix smae? {is_diff}\n")
-
+rel_errors = errors/y
 print(f"Size of errors: {errors.shape}")
 print(f"L2 norm of errors: {np.linalg.norm(errors)}")
-print(f"L2 norm of relative errors: {np.linalg.noprm(rel_errors)}")
+print(f"L2 norm of relative errors: {np.linalg.norm(rel_errors)}")
 
 
 # the optimization problem here is to find the coefficients that minimise the mean squared error
@@ -70,17 +72,19 @@ prediction_model = X @ coefs
 
 errors_model = y - prediction_model
 
-rel_errorts_model = errors_model/y
-print(f"L2 norm of error_model: (np.linalg.norm)")
+rel_errors_model = errors_model/y
+print(f"L2 norm of error_model: {(np.linalg.norm(rel_errors_model))}")
 
 
 X = df.drop("Price", axis =1).values
 y = df["Price"].values
 
 n_samples, n_features = X.shape
-print(f"Number of samples, features: ")
-X = df.drop(np.ones(n.samples))
+print(f"Number of samples, features: {n_samples, n_features}")
 
+X = np.hstack((np.ones((n_samples, 1)), X))
+coefs = np.linalg.inv(X.T @ X) @  X.T @ y
+np.savetxt("coefs_all.csv", coefs, delimiter=",")
 
 rank_XTX = np.linalg.matrix_rank(X.T @ X)
 print(f"Rank of X.T @ X: {rank_XTX}")
@@ -93,7 +97,7 @@ np.savetxt("R.csv", R, delimiter=",")
 
 # R*coeffs = b
 
-sol = Q.T * Q
+sol = Q.T @ Q
 np.savetxt("sol.csv", sol,delimiter=",")
 
 # X = QR
@@ -113,14 +117,26 @@ for i in range(n_features, -1, -1):
 np.savetxt("coeffs_qr_loop.csv", coeffs_qr_loop, delimiter=",")
 
 # solve normal eq using svd X = U S V^T
-U, S, Vt = np.linalg.svd(X, full_matrices=False)
+# U, S, Vt = np.linalg.svd(X, full_matrices=False)
+U, S, Vt = np.linalg.svd(X)
 
 # find the inverse of X in least squares sense (Pseudo inverse of X)
 
 #Xdagger = (X^T X)^-1 X^T
 
 #complete caluculate the coeffs
+print(Vt.shape, S.T.shape, U.T.shape)
+Sp = 1/(S*S)
+spdiag = np.diag(Sp)
+S = np.diag(S)
+#print(S)
+S = np.vstack((S, np.zeros((500-12,12))))
 
 
+print(Vt.shape, S.T.shape, U.T.shape)
 
+# coeff_svd = Vt.T @ np.linalg.inv(S.T @ S) @ S.T @ U.T @y
+coeff_svd = Vt.T @ (spdiag) @ S.T @ U.T @y
+
+np.savetxt("coef_svd.csv",coeff_svd, delimiter=',' )
 
